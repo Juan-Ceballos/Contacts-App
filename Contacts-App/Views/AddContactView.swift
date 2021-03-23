71f429a612c00bf04ce8357a31b2b11f46146082
Juan C <juanceballos@pursuit.org>
2021-03-22 22:09:23 -0400

    add contact added entry field views on a scrollview

--- Contacts-App/Views/AddContactView.swift
+++ Contacts-App/Views/AddContactView.swift
@@ -9,42 +9,127 @@ import UIKit
 
 class AddContactView: UIView {
     
-    lazy var someHeight = CGSize(width: self.frame.size.width, height: self.frame.size.height + 300)
-    
     override func layoutSubviews() {
-        
+        containerView.roundCorners()
     }
     
     public lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
-        scrollView.backgroundColor = .purple
-        scrollView.frame = self.bounds
-        scrollView.contentSize = someHeight
+        scrollView.backgroundColor = .systemBackground
+        scrollView.automaticallyAdjustsScrollIndicatorInsets = false
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+        scrollView.contentOffset = CGPoint(x: 0.0, y: 0.0)
+        scrollView.contentInsetAdjustmentBehavior = .never
         return scrollView
     }()
     
     public lazy var containerView: UIView = {
         let view = UIView()
+        view.backgroundColor = .systemFill
+        return view
+    }()
+    
+    public lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name"
+        label.textColor = UIColor(named: "MyTextColor")
+        label.font = UIFont(name: "GillSans", size: 22)
+        return label
+    }()
+    
+    public lazy var firstNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "First"
+        return textField
+    }()
+    
+    public lazy var lastNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Last"
+        return textField
+    }()
+    
+    public lazy var poNumberLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Phone Number"
+        label.textColor = UIColor(named: "MyTextColor")
+        label.font = UIFont(name: "GillSans", size: 22)
+        return label
+    }()
+    
+    public lazy var poNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Digits"
+        return textField
+    }()
+    
+    public lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email"
+        label.textColor = UIColor(named: "MyTextColor")
+        label.font = UIFont(name: "GillSans", size: 22)
+        return label
+    }()
+    
+    public lazy var emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Johny@example.com"
+        return textField
+    }()
+    
+    public lazy var addressLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Address"
+        label.textColor = UIColor(named: "MyTextColor")
+        label.font = UIFont(name: "GillSans", size: 22)
+        return label
+    }()
+    
+    public lazy var streetTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Street Address"
+        return textField
+    }()
+    
+    public lazy var aptTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "apt"
+        return textField
+    }()
+    
+    public lazy var cityTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "City"
+        return textField
+    }()
+    
+    public lazy var stateTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "State"
+        return textField
+    }()
+    
+    public lazy var zipTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Zip Code"
+        return textField
+    }()
+    
+    public lazy var someView: UIView = {
+        let view = UIView()
         view.backgroundColor = .systemTeal
-        view.frame.size = someHeight
-        return view
-    }()
-    
-    public lazy var sampleView1: UIView = {
-        let view = UIView()
-        view.backgroundColor = .red
-        return view
-    }()
-    
-    public lazy var sampleView2: UIView = {
-        let view = UIView()
-        view.backgroundColor = .yellow
-        return view
-    }()
-    
-    public lazy var sampleView3: UIView = {
-        let view = UIView()
-        view.backgroundColor = .green
+        view.layer.borderWidth = 10
+        view.layer.borderColor = UIColor.red.cgColor
         return view
     }()
     
@@ -61,9 +146,20 @@ class AddContactView: UIView {
     private func commonInit()   {
         setupScrollViewConstraints()
         setupContainerViewConstraints()
-        setupSampleView1Constraints()
-        setupSampleView2Constraints()
-        setupSampleView3Constraints()
+        setupNameLabelConstraints()
+        setupFirstNameTextFieldConstraints()
+        setupLastNameTextFieldConstraints()
+        setupPoNumberLabelConstraints()
+        setupPoNumberTextFieldConstraints()
+        setupEmailLabelConstraints()
+        setupEmailTextFieldConstraints()
+        setupAddressLabelConstraints()
+        setupStreetTextFieldConstraints()
+        setupAptTextFieldConstraints()
+        setupCityTextFieldConstraints()
+        setupStateTextFieldConstraints()
+        setupZipTextFieldConstraints()
+        setupSomeViewConstraints()
     }
     
     private func setupScrollViewConstraints() {
@@ -74,7 +170,7 @@ class AddContactView: UIView {
             scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
             scrollView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
-            scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor)
+            scrollView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
         ])
     }
     
@@ -84,62 +180,177 @@ class AddContactView: UIView {
         containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
-            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
-            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
-            containerView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            containerView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
-            containerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
-            containerView.heightAnchor.constraint(equalTo: scrollView.heightAnchor, constant: 300)
+            containerView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 11),
+            containerView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -11),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -11),
+            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 11),
+            containerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
         ])
     }
     
-    private func setupSampleView1Constraints() {
-        containerView.addSubview(sampleView1)
-        
-        sampleView1.translatesAutoresizingMaskIntoConstraints = false
-        
+    private func setupNameLabelConstraints() {
+        containerView.addSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
         
-            sampleView1.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
-            sampleView1.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.3),
-            sampleView1.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 8),
-            sampleView1.heightAnchor.constraint(equalToConstant: 400)
+            nameLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 11),
+            nameLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
         
         ])
     }
     
-    private func setupSampleView2Constraints() {
-        containerView.addSubview(sampleView2)
-        
-        sampleView2.translatesAutoresizingMaskIntoConstraints = false
-
-        
+    private func setupFirstNameTextFieldConstraints() {
+        containerView.addSubview(firstNameTextField)
+        firstNameTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
         
-            sampleView2.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
-            sampleView2.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.3),
-            sampleView2.topAnchor.constraint(equalTo: sampleView1.bottomAnchor, constant: 8),
-            sampleView2.heightAnchor.constraint(equalToConstant: 400)
+            firstNameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 11),
+            firstNameTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
+            firstNameTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3)
         
         ])
     }
     
-    private func setupSampleView3Constraints() {
-        containerView.addSubview(sampleView3)
-        
-        sampleView3.translatesAutoresizingMaskIntoConstraints = false
-
-        
+    private func setupLastNameTextFieldConstraints() {
+        containerView.addSubview(lastNameTextField)
+        lastNameTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
         
-            sampleView3.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 8),
-            sampleView3.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 0.3),
-            sampleView3.topAnchor.constraint(equalTo: sampleView2.bottomAnchor, constant: 8),
-            sampleView3.heightAnchor.constraint(equalToConstant: 400),
-            sampleView3.bottomAnchor.constraint(equalTo: containerView.bottomAnchor)
+            lastNameTextField.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 11),
+            lastNameTextField.leadingAnchor.constraint(equalTo: firstNameTextField.trailingAnchor, constant: 11),
+            lastNameTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3)
+    
+        ])
+    }
+    
+    private func setupPoNumberLabelConstraints() {
+        containerView.addSubview(poNumberLabel)
+        poNumberLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
         
+            poNumberLabel.topAnchor.constraint(equalTo: lastNameTextField.bottomAnchor, constant: 22),
+            poNumberLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11)
+        
+        ])
+    }
+    
+    private func setupPoNumberTextFieldConstraints() {
+        containerView.addSubview(poNumberTextField)
+        poNumberTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            poNumberTextField.topAnchor.constraint(equalTo: poNumberLabel.bottomAnchor, constant: 11),
+            poNumberTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
+            poNumberTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5)
+        
+        ])
+    }
+    
+    private func setupEmailLabelConstraints() {
+        containerView.addSubview(emailLabel)
+        emailLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            emailLabel.topAnchor.constraint(equalTo: poNumberTextField.bottomAnchor, constant: 22),
+            emailLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11)
+        
+        ])
+    }
+    
+    private func setupEmailTextFieldConstraints() {
+        containerView.addSubview(emailTextField)
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            emailTextField.topAnchor.constraint(equalTo: emailLabel.bottomAnchor, constant: 11),
+            emailTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
+            emailTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5)
+        
+        ])
+    }
+    
+    private func setupAddressLabelConstraints() {
+        containerView.addSubview(addressLabel)
+        addressLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            addressLabel.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 22),
+            addressLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
+        
+        ])
+    }
+    
+    private func setupStreetTextFieldConstraints() {
+        containerView.addSubview(streetTextField)
+        streetTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            streetTextField.topAnchor.constraint(equalTo: addressLabel.bottomAnchor, constant: 11),
+            streetTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
+            streetTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.6),
+        
+        ])
+    }
+    
+    private func setupAptTextFieldConstraints() {
+        containerView.addSubview(aptTextField)
+        aptTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            aptTextField.leadingAnchor.constraint(equalTo: streetTextField.trailingAnchor, constant: 11),
+            aptTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.2),
+            aptTextField.topAnchor.constraint(equalTo: addressLabel.bottomAnchor, constant: 11)
+        
+        ])
+    }
+    
+    private func setupCityTextFieldConstraints() {
+        containerView.addSubview(cityTextField)
+        cityTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            cityTextField.topAnchor.constraint(equalTo: streetTextField.bottomAnchor, constant: 11),
+            cityTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
+            cityTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.4)
+        
+        ])
+    }
+    
+    private func setupStateTextFieldConstraints() {
+        containerView.addSubview(stateTextField)
+        stateTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            stateTextField.topAnchor.constraint(equalTo: streetTextField.bottomAnchor, constant: 11),
+            stateTextField.leadingAnchor.constraint(equalTo: cityTextField.trailingAnchor, constant: 11),
+            stateTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.2)
+        
+        ])
+    }
+    
+    private func setupZipTextFieldConstraints() {
+        containerView.addSubview(zipTextField)
+        zipTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            zipTextField.topAnchor.constraint(equalTo: cityTextField.bottomAnchor, constant: 11),
+            zipTextField.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
+            zipTextField.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3),
+            //zipTextField.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -11)
+        ])
+    }
+    
+    private func setupSomeViewConstraints() {
+        containerView.addSubview(someView)
+        someView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+        
+            someView.topAnchor.constraint(equalTo: zipTextField.bottomAnchor, constant: 11),
+            someView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 11),
+            someView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.3),
+            someView.heightAnchor.constraint(equalToConstant: 400),
+            someView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -11)
+            
         ])
     }
     
